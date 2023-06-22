2e98f4ba819cfc4cd3dce6c0d118a289b7016162
AltheaGemino <althea.gemino@obf.ateneo.edu>
2023-06-22 18:54:32 +0800

    uploaded ipa-1

--- ipa-1.py
+++ ipa-1.py
@@ -4,7 +4,7 @@
 
 This assignment will develop your basic familiarity with Python.
 '''
-
+ 
 def savings(gross_pay, tax_rate, expenses):
     '''Savings.
     5 points.
@@ -31,9 +31,9 @@ def savings(gross_pay, tax_rate, expenses):
     int
         the number of centavos remaining from an employee's pay after taxes and expenses
     '''
-    # Replace `pass` with your code.
-    # Stay within the function. Only use the parameters as input. The function should return your answer.
-    pass
+    after_taxpay = round(gross_pay*(1-tax_rate))
+    final_pay = after_taxpay - expenses 
+    return int(final_pay) 
 
 def material_waste(total_material, material_units, num_jobs, job_consumption):
     '''Material Waste.
@@ -66,9 +66,9 @@ def material_waste(total_material, material_units, num_jobs, job_consumption):
     str
         the amount of remaining material expressed with its unit (e.g., "10kg").
     '''
-    # Replace `pass` with your code.
-    # Stay within the function. Only use the parameters as input. The function should return your answer.
-    pass
+    consumed_material = num_jobs * job_consumption
+    remaining_material = total_material - consumed_material
+    return str(remaining_material) + material_units
 
 def interest(principal, rate, periods):
     '''Interest.
@@ -96,9 +96,9 @@ def interest(principal, rate, periods):
     int
         the final value of the investment
     '''
-    # Replace `pass` with your code.
-    # Stay within the function. Only use the parameters as input. The function should return your answer.
-    pass
+    interest = rate * periods
+    final_value= principal + interest
+    return int(final_value)
 
 def body_mass_index(weight, height):
     '''Body Mass Index.
@@ -132,6 +132,6 @@ def body_mass_index(weight, height):
     float
         the BMI of the person.
     '''
-    # Replace `pass` with your code.
-    # Stay within the function. Only use the parameters as input. The function should return your answer.
-    pass
+    weight_kg = weight / 2.2046 
+    height_m = height[0] / 3.281 + height[1] / 39.37 
+    return weight_kg / (height_m**2) 
